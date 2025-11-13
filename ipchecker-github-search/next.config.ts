523f320a7c2ae365783f2d65b4eebbe861feec7b
Seva <20211206nch@mail.ru>
2025-11-13 21:58:03 +0300

    feat: cpnfig ts+next

---
+++ ipchecker-github-search/next.config.ts
@@ -0,0 +1,12 @@
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
